4cfe0f5b446777cc570c540e92da7d639367dbaf
xcc <xcc@kfb.cn>
2015-09-29 08:28:03 +0800

    添加itcdatastruct及itctypes文件

---
+++ ImageBasedIibrary/ImageBasedIibrary/itcdatastructs.cpp
@@ -0,0 +1,111 @@
+#include "itctypes.h"
+#include "limits.h"
+#include <assert.h>
+#include <iostream>
+
+
+/* maximum size of dynamic memory buffer.
+   cvAlloc reports an error if a larger block is requested. */
+#define  ITC_MAX_ALLOC_SIZE    (((size_t)1 << (sizeof(size_t)*8-2)))
+/* default storage block size */
+#define  ITC_STORAGE_BLOCK_SIZE   ((1<<16) - 128)
+/* default alignment for dynamic data strucutures, resided in storages. */
+#define  ITC_STRUCT_ALIGN    ((int)sizeof(double))
+
+//error code
+#define  ITC_StsOutOfRange -211  /* some of parameters are out of range */
+#define  ITC_StsNoMem -4	/* insufficient memory */
+#define  ITC_StsNullPtr  -27 /* null pointer */
+
+/* the alignment of all the allocated buffers */
+#define  ITC_MALLOC_ALIGN    32
+
+// pointers to allocation functions, initially set to default
+static void* p_cvAllocUserData = 0;
+
+inline int  itcAlign(int size, int align)
+{
+	assert((align&(align-1))==0 && size<INT_MAX);
+	return (size + align - 1) & -align;
+}
+
+inline void* itcAlignPtr( const void* ptr, int align=32 )
+{
+	assert( (align & (align-1)) == 0 );
+	return (void*)( ((size_t)ptr + align - 1) & ~(size_t)(align-1) );
+}
+
+// default <malloc>
+static void*
+	icvDefaultAlloc( size_t size, void* )
+{
+	char *ptr, *ptr0 = (char*)malloc(
+		(size_t)(size + ITC_MALLOC_ALIGN*((size >= 4096) + 1) + sizeof(char*)));
+
+	if( !ptr0 )
+		return 0;
+
+	// align the pointer
+	ptr = (char*)itcAlignPtr(ptr0 + sizeof(char*) + 1, ITC_MALLOC_ALIGN);
+	*(char**)(ptr - sizeof(char*)) = ptr0;
+
+	return ptr;
+}
+
+// pointers to allocation functions, initially set to default
+//static CvAllocFunc p_cvAlloc = icvDefaultAlloc;
+
+void*  itcAlloc( size_t size )
+{
+	void* ptr = 0;
+
+	//CV_FUNCNAME( "cvAlloc" );
+
+	//__BEGIN__;
+
+	if( (size_t)size > ITC_MAX_ALLOC_SIZE )
+		std::cout<<"err: "<<ITC_StsOutOfRange<<std::endl;
+
+	ptr = icvDefaultAlloc( size, p_cvAllocUserData );
+	if( !ptr )
+		std::cout<<"err: "<<ITC_StsNoMem<<std::endl;
+
+	//__END__;
+
+	return ptr;
+}
+
+/* initializes allocated storage */
+static void itcInitMemStorage( ItcMemStorage* storage, int block_size )
+{
+	//CV_FUNCNAME( "icvInitMemStorage " );
+
+	//__BEGIN__;
+
+	if( !storage )
+		std::cout<<"err: "<<ITC_StsNullPtr<<std::endl;
+
+	if( block_size <= 0 )
+		block_size = ITC_STORAGE_BLOCK_SIZE;
+
+	block_size = itcAlign( block_size, ITC_STRUCT_ALIGN );
+	assert( sizeof(ItcMemBlock) % ITC_STRUCT_ALIGN == 0 );
+
+	memset( storage, 0, sizeof( *storage ));
+	storage->signature = ITC_STORAGE_MAGIC_VAL;
+	storage->block_size = block_size;
+
+	//__END__;
+}
+
+/* creates root memory storage */
+ItcMemStorage* itcCreateMemStorage(int block_size)
+{
+	ItcMemStorage *storage = 0;
+
+	storage=(ItcMemStorage *) itcAlloc(sizeof(ItcMemStorage));
+
+	itcInitMemStorage( storage, block_size );
+
+	return storage;
+}
---
+++ ImageBasedIibrary/ImageBasedIibrary/itctypes.h
@@ -0,0 +1,180 @@
+/****************************************************************************************\
+*                             Common macros and inline functions                         *
+\****************************************************************************************/
+
+inline int itcRound(double a)
+{
+	return (int)(a+0.5);
+}
+
+/*************************************** ItcRect *****************************************/
+
+typedef struct ItcRect
+{
+	int x;
+	int y;
+	int width;
+	int height;
+}ItcRect;
+
+inline ItcRect  itcRect(int x, int y, int width, int height)
+{
+	ItcRect r;
+
+	r.x=x;
+	r.y=y;
+	r.width=width;
+	r.height=height;
+
+	return r;
+}
+
+/*************************************** ItcPoint *****************************************/
+
+typedef struct ItcPoint
+{
+	int x;
+	int y;
+}ItcPoint;
+
+inline ItcPoint itcPoint(int x, int y)
+{
+	ItcPoint p;
+	p.x=x;
+	p.y=y;
+
+	return p;
+}
+
+typedef struct ItcPoint2D32f
+{
+	float x;
+	float y;
+}ItcPoint2D32f;
+
+
+inline  ItcPoint2D32f  itcPoint2D32f( double x, double y )
+{
+	ItcPoint2D32f p;
+
+	p.x = (float)x;
+	p.y = (float)y;
+
+	return p;
+}
+
+
+inline  ItcPoint2D32f  itcPointTo32f( ItcPoint point )
+{
+	return itcPoint2D32f( (float)point.x, (float)point.y );
+}
+
+
+inline  ItcPoint  itcPointFrom32f( ItcPoint2D32f point )
+{
+	ItcPoint ipt;
+	ipt.x = itcRound(point.x);
+	ipt.y = itcRound(point.y);
+
+	return ipt;
+}
+
+
+typedef struct ItcPoint3D32f
+{
+	float x;
+	float y;
+	float z;
+}ItcPoint3D32f;
+
+
+inline  ItcPoint3D32f  itcPoint3D32f( double x, double y, double z )
+{
+	ItcPoint3D32f p;
+
+	p.x = (float)x;
+	p.y = (float)y;
+	p.z = (float)z;
+
+	return p;
+}
+
+
+typedef struct ItcPoint2D64f
+{
+	double x;
+	double y;
+}ItcPoint2D64f;
+
+
+inline  ItcPoint2D64f  itcPoint2D64f( double x, double y )
+{
+	ItcPoint2D64f p;
+
+	p.x = x;
+	p.y = y;
+
+	return p;
+}
+
+
+typedef struct ItcPoint3D64f
+{
+	double x;
+	double y;
+	double z;
+}
+ItcPoint3D64f;
+
+
+inline  ItcPoint3D64f  itcPoint3D64f( double x, double y, double z )
+{
+	ItcPoint3D64f p;
+
+	p.x = x;
+	p.y = y;
+	p.z = z;
+
+	return p;
+}
+
+/******************************** CvSize's & CvBox **************************************/
+
+typedef struct ItcSize
+{
+	int width;
+	int height;
+}ItcSize;
+
+inline ItcSize itcSize(int width, int height)
+{
+	ItcSize s;
+	s.width=width;
+	s.height=height;
+}
+
+
+/****************************************************************************************\
+*                                   Dynamic Data structures                              *
+\****************************************************************************************/
+
+/******************************** Memory storage ****************************************/
+
+typedef struct ItcMemBlock
+{
+	struct ItcMemBlock*  prev;
+	struct ItcMemBlock*  next;
+}
+ItcMemBlock;
+
+#define ITC_STORAGE_MAGIC_VAL    0x42890000
+
+typedef struct ItcMemStorage
+{
+	int signature;
+	ItcMemBlock* bottom;/* first allocated block */
+	ItcMemBlock* top;   /* current memory block - top of the stack */
+	struct  ItcMemStorage* parent; /* borrows new blocks from */
+	int block_size;  /* block size */
+	int free_space;  /* free space in the current block */
+}ItcMemStorage;
