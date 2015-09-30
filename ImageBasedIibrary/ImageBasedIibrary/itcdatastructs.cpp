bfd2914a310c69044913cfb755097ed185ebe900
xcc <xcc@kfb.cn>
2015-09-30 15:14:42 +0800

    整理itcdatastructs.cpp和itctypes.h

--- ImageBasedIibrary/ImageBasedIibrary/itcdatastructs.cpp
+++ ImageBasedIibrary/ImageBasedIibrary/itcdatastructs.cpp
@@ -24,7 +24,12 @@
 /* 0x3a50 = 11 10 10 01 01 00 00 ~ array of log2(sizeof(arr_type_elem)) */
 #define ITC_ELEM_SIZE(type) \
 	(ITC_MAT_CN(type) << ((((sizeof(size_t)/4+1)*16384|0x3a50) >> ITC_MAT_DEPTH(type)*2) & 3))
-
+#define ITC_SHIFT_TAB_MAX 32
+static const char itcPower2ShiftTab[] =
+{
+	0, 1, -1, 2, -1, -1, -1, 3, -1, -1, -1, -1, -1, -1, -1, 4,
+	-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 5
+};
 
 
 //error code
@@ -37,9 +42,31 @@
 
 /* the alignment of all the allocated buffers */
 #define  ITC_MALLOC_ALIGN    32
-
 // pointers to allocation functions, initially set to default
 static void* p_cvAllocUserData = 0;
+
+// declaration
+inline int  itcAlign(int size, int align);
+inline void* itcAlignPtr( const void* ptr, int align );
+inline int itcAlignLeft( int size, int align );
+static void* itcDefaultAlloc( size_t size, void* );
+static int itcDefaultFree( void* ptr, void* );
+void*  itcAlloc( size_t size );
+void  itcFree_( void* ptr );
+static void itcInitMemStorage( ItcMemStorage* storage, int block_size );
+ItcMemStorage* itcCreateMemStorage(int block_size);
+ItcMemStorage* itcCreateChildMemStorage( ItcMemStorage * parent );
+static void itcDestroyMemStorage( ItcMemStorage* storage );
+void itcReleaseMemStorage( ItcMemStorage** storage );
+void itcClearMemStorage( ItcMemStorage * storage );
+static void itcGoNextMemBlock( ItcMemStorage * storage );
+void itcSaveMemStoragePos( const ItcMemStorage * storage, ItcMemStoragePos * pos );
+void itcRestoreMemStoragePos( ItcMemStorage * storage, ItcMemStoragePos * pos );
+void* itcMemStorageAlloc( ItcMemStorage* storage, size_t size );
+ItcSeq *itcCreateSeq( int seq_flags, int header_size, int elem_size, ItcMemStorage * storage );
+void itcSetSeqBlockSize( ItcSeq *seq, int delta_elements );
+char* itcGetSeqElem( const ItcSeq *seq, int index );
+int itcSeqElemIdx( const ItcSeq* seq, const void* _element, ItcSeqBlock** _block );
 
 inline int  itcAlign(int size, int align)
 {
@@ -58,6 +85,8 @@ inline int
 {
 	return size & -align;
 }
+
+
 
 // default <malloc>
 static void*
@@ -546,3 +575,283 @@ itcSetSeqBlockSize( ItcSeq *seq, int delta_elements )
 
     //__END__;
 }
+
+/* finds sequence element by its index */
+char*
+	itcGetSeqElem( const ItcSeq *seq, int index )
+{
+	ItcSeqBlock *block;
+	int count, total = seq->total;
+
+	if( (unsigned)index >= (unsigned)total )
+	{
+		index += index < 0 ? total : 0;
+		index -= index >= total ? total : 0;
+		if( (unsigned)index >= (unsigned)total )
+			return 0;
+	}
+
+	block = seq->first;
+	if( index + index <= total )
+	{
+		while( index >= (count = block->count) )
+		{
+			block = block->next;
+			index -= count;
+		}
+	}
+	else
+	{
+		do
+		{
+			block = block->prev;
+			total -= block->count;
+		}
+		while( index < total );
+		index -= total;
+	}
+
+	return block->data + index * seq->elem_size;
+}
+
+/* calculates index of sequence element */
+int
+	itcSeqElemIdx( const ItcSeq* seq, const void* _element, ItcSeqBlock** _block )
+{
+	const char *element = (const char *)_element;
+	int elem_size;
+	int id = -1;
+	ItcSeqBlock *first_block;
+	ItcSeqBlock *block;
+
+	//CV_FUNCNAME( "cvSeqElemIdx" );
+
+	//__BEGIN__;
+
+	if( !seq || !element )
+		std::cout<<"err: "<<ITC_StsNullPtr<<std::endl;
+
+	block = first_block = seq->first;
+	elem_size = seq->elem_size;
+
+	for( ;; )
+	{
+		if( (unsigned)(element - block->data) < (unsigned) (block->count * elem_size) )
+		{
+			if( _block )
+				*_block = block;
+			if( elem_size <= ITC_SHIFT_TAB_MAX && (id = itcPower2ShiftTab[elem_size - 1]) >= 0 )
+				id = (int)((size_t)(element - block->data) >> id);
+			else
+				id = (int)((size_t)(element - block->data) / elem_size);
+			id += block->start_index - seq->first->start_index;
+			break;
+		}
+		block = block->next;
+		if( block == first_block )
+			break;
+	}
+
+	//__END__;
+
+	return id;
+}
+
+/* pushes element to the sequence */
+char*
+	cvSeqPush( ItcSeq *seq, void *element )
+{
+	char *ptr = 0;
+	size_t elem_size;
+
+	//CV_FUNCNAME( "cvSeqPush" );
+
+	//__BEGIN__;
+
+	if( !seq )
+		std::cout<<"err: "<<ITC_StsNullPtr<<std::endl;
+		//CV_ERROR( CV_StsNullPtr, "" );
+
+	elem_size = seq->elem_size;
+	ptr = seq->ptr;
+
+	if( ptr >= seq->block_max )
+	{
+		//CV_CALL( icvGrowSeq( seq, 0 ));
+		itcGrowSeq( seq, 0 );
+		ptr = seq->ptr;
+		assert( ptr + elem_size <= seq->block_max /*&& ptr == seq->block_min */  );
+	}
+
+	if( element )
+		CV_MEMCPY_AUTO( ptr, element, elem_size );
+	seq->first->prev->count++;
+	seq->total++;
+	seq->ptr = ptr + elem_size;
+
+	__END__;
+
+	return ptr;
+}
+
+
+/* the function allocates space for at least one more sequence element.
+   if there are free sequence blocks (seq->free_blocks != 0),
+   they are reused, otherwise the space is allocated in the storage */
+static void
+itcGrowSeq( ItcSeq *seq, int in_front_of )
+{
+    //CV_FUNCNAME( "icvGrowSeq" );
+
+    //__BEGIN__;
+
+    ItcSeqBlock *block;
+
+    if( !seq )
+		std::cout<<"err: "<<ITC_StsNullPtr<<std::endl;
+        //CV_ERROR( CV_StsNullPtr, "" );
+    block = seq->free_blocks;
+
+    if( !block )
+    {
+        int elem_size = seq->elem_size;
+        int delta_elems = seq->delta_elems;
+        ItcMemStorage *storage = seq->storage;
+
+        if( seq->total >= delta_elems*4 )
+            itcSetSeqBlockSize( seq, delta_elems*2 );
+
+        if( !storage )
+			std::cout<<"err: "<<ITC_StsNullPtr<<std::endl;
+            //CV_ERROR( CV_StsNullPtr, "The sequence has NULL storage pointer" );
+
+        /* if there is a free space just after last allocated block
+           and it's big enough then enlarge the last block
+           (this can happen only if the new block is added to the end of sequence */
+        if( (unsigned)(ICV_FREE_PTR(storage) - seq->block_max) < ITC_STRUCT_ALIGN &&
+            storage->free_space >= seq->elem_size && !in_front_of )
+        {
+            int delta = storage->free_space / elem_size;
+
+            delta = ITC_MIN( delta, delta_elems ) * elem_size;
+            seq->block_max += delta;
+            storage->free_space = itcAlignLeft((int)(((char*)storage->top + storage->block_size) -
+                                              seq->block_max), ITC_STRUCT_ALIGN );
+            //EXIT;
+        }
+        else
+        {
+            int delta = elem_size * delta_elems + (int)itcAlign(sizeof(ItcSeqBlock), ITC_STRUCT_ALIGN);
+
+            /* try to allocate <delta_elements> elements */
+            if( storage->free_space < delta )
+            {
+                int small_block_size = ITC_MAX(1, delta_elems/3)*elem_size +
+                                       (int)itcAlign(sizeof(ItcSeqBlock), ITC_STRUCT_ALIGN);
+                /* try to allocate smaller part */
+                if( storage->free_space >= small_block_size + ITC_STRUCT_ALIGN )
+                {
+                    delta = (storage->free_space - (int)itcAlign(sizeof(ItcSeqBlock), ITC_STRUCT_ALIGN))/seq->elem_size;
+                    delta = delta*seq->elem_size + (int)itcAlign(sizeof(ItcSeqBlock), ITC_STRUCT_ALIGN);
+                }
+                else
+                {
+                    //CV_CALL( icvGoNextMemBlock( storage ));
+					itcGoNextMemBlock(storage);
+                    assert( storage->free_space >= delta );
+                }
+            }
+
+            //CV_CALL( block = (CvSeqBlock*)cvMemStorageAlloc( storage, delta ));
+			block = (ItcSeqBlock*)itcMemStorageAlloc( storage, delta );
+            block->data = (char*)itcAlignPtr( block + 1, ITC_STRUCT_ALIGN );
+            block->count = delta - (int)itcAlign(sizeof(ItcSeqBlock), ITC_STRUCT_ALIGN);
+            block->prev = block->next = 0;
+        }
+    }
+    else
+    {
+        seq->free_blocks = block->next;
+    }
+
+    if( !(seq->first) )
+    {
+        seq->first = block;
+        block->prev = block->next = block;
+    }
+    else
+    {
+        block->prev = seq->first->prev;
+        block->next = seq->first;
+        block->prev->next = block->next->prev = block;
+    }
+
+    /* for free blocks the <count> field means total number of bytes in the block.
+       And for used blocks it means a current number of sequence
+       elements in the block */
+    assert( block->count % seq->elem_size == 0 && block->count > 0 );
+
+    if( !in_front_of )
+    {
+        seq->ptr = block->data;
+        seq->block_max = block->data + block->count;
+        block->start_index = block == block->prev ? 0 :
+            block->prev->start_index + block->prev->count;
+    }
+    else
+    {
+        int delta = block->count / seq->elem_size;
+        block->data += block->count;
+
+        if( block != block->prev )
+        {
+            assert( seq->first->start_index == 0 );
+            seq->first = block;
+        }
+        else
+        {
+            seq->block_max = seq->ptr = block->data;
+        }
+
+        block->start_index = 0;
+
+        for( ;; )
+        {
+            block->start_index += delta;
+            block = block->next;
+            if( block == seq->first )
+                break;
+        }
+    }
+
+    block->count = 0;
+
+    //__END__;
+}
+//int
+//itcSliceLength( CvSlice slice, const CvSeq* seq )
+//{
+//    int total = seq->total;
+//    int length = slice.end_index - slice.start_index;
+//    
+//    if( length != 0 )
+//    {
+//        if( slice.start_index < 0 )
+//            slice.start_index += total;
+//        if( slice.end_index <= 0 )
+//            slice.end_index += total;
+//
+//        length = slice.end_index - slice.start_index;
+//    }
+//
+//    if( length < 0 )
+//    {
+//        length += total;
+//        /*if( length < 0 )
+//            length += total;*/
+//    }
+//    else if( length > total )
+//        length = total;
+//
+//    return length;
+//}
--- ImageBasedIibrary/ImageBasedIibrary/itctypes.h
+++ ImageBasedIibrary/ImageBasedIibrary/itctypes.h
@@ -3,19 +3,86 @@
 \****************************************************************************************/
 
 
-#define CV_MAT_CN_MASK          ((CV_CN_MAX - 1) << CV_CN_SHIFT)
-#define CV_MAT_CN(flags)        ((((flags) & CV_MAT_CN_MASK) >> CV_CN_SHIFT) + 1)
-#define CV_MAT_DEPTH_MASK       (CV_DEPTH_MAX - 1)
-#define CV_MAT_DEPTH(flags)     ((flags) & CV_MAT_DEPTH_MASK)
-#define CV_MAT_TYPE_MASK        (CV_DEPTH_MAX*CV_CN_MAX - 1)
-#define CV_MAT_TYPE(flags)      ((flags) & CV_MAT_TYPE_MASK)
-#define CV_MAT_CONT_FLAG_SHIFT  14
-#define CV_MAT_CONT_FLAG        (1 << CV_MAT_CONT_FLAG_SHIFT)
-#define CV_IS_MAT_CONT(flags)   ((flags) & CV_MAT_CONT_FLAG)
-#define CV_IS_CONT_MAT          CV_IS_MAT_CONT
-#define CV_MAT_TEMP_FLAG_SHIFT  15
-#define CV_MAT_TEMP_FLAG        (1 << CV_MAT_TEMP_FLAG_SHIFT)
-#define CV_IS_TEMP_MAT(flags)   ((flags) & CV_MAT_TEMP_FLAG)
+#define ITC_CN_MAX     64
+#define ITC_CN_SHIFT   3
+#define ITC_DEPTH_MAX  (1 << ITC_CN_SHIFT)
+
+#define ITC_8U   0
+#define ITC_8S   1
+#define ITC_16U  2
+#define ITC_16S  3
+#define ITC_32S  4
+#define ITC_32F  5
+#define ITC_64F  6
+#define ITC_USRTYPE1 7
+
+#define ITC_MAKETYPE(depth,cn) ((depth) + (((cn)-1) << ITC_CN_SHIFT))
+#define ITC_MAKE_TYPE ITC_MAKETYPE
+
+#define ITC_8UC1 ITC_MAKETYPE(ITC_8U,1)
+#define ITC_8UC2 ITC_MAKETYPE(ITC_8U,2)
+#define ITC_8UC3 ITC_MAKETYPE(ITC_8U,3)
+#define ITC_8UC4 ITC_MAKETYPE(ITC_8U,4)
+#define ITC_8UC(n) ITC_MAKETYPE(ITC_8U,(n))
+
+#define ITC_8SC1 ITC_MAKETYPE(ITC_8S,1)
+#define ITC_8SC2 ITC_MAKETYPE(ITC_8S,2)
+#define ITC_8SC3 ITC_MAKETYPE(ITC_8S,3)
+#define ITC_8SC4 ITC_MAKETYPE(ITC_8S,4)
+#define ITC_8SC(n) ITC_MAKETYPE(ITC_8S,(n))
+
+#define ITC_16UC1 ITC_MAKETYPE(ITC_16U,1)
+#define ITC_16UC2 ITC_MAKETYPE(ITC_16U,2)
+#define ITC_16UC3 ITC_MAKETYPE(ITC_16U,3)
+#define ITC_16UC4 ITC_MAKETYPE(ITC_16U,4)
+#define ITC_16UC(n) ITC_MAKETYPE(ITC_16U,(n))
+
+#define ITC_16SC1 ITC_MAKETYPE(ITC_16S,1)
+#define ITC_16SC2 ITC_MAKETYPE(ITC_16S,2)
+#define ITC_16SC3 ITC_MAKETYPE(ITC_16S,3)
+#define ITC_16SC4 ITC_MAKETYPE(ITC_16S,4)
+#define ITC_16SC(n) ITC_MAKETYPE(ITC_16S,(n))
+
+#define ITC_32SC1 ITC_MAKETYPE(ITC_32S,1)
+#define ITC_32SC2 ITC_MAKETYPE(ITC_32S,2)
+#define ITC_32SC3 ITC_MAKETYPE(ITC_32S,3)
+#define ITC_32SC4 ITC_MAKETYPE(ITC_32S,4)
+#define ITC_32SC(n) ITC_MAKETYPE(ITC_32S,(n))
+
+#define ITC_32FC1 ITC_MAKETYPE(ITC_32F,1)
+#define ITC_32FC2 ITC_MAKETYPE(ITC_32F,2)
+#define ITC_32FC3 ITC_MAKETYPE(ITC_32F,3)
+#define ITC_32FC4 ITC_MAKETYPE(ITC_32F,4)
+#define ITC_32FC(n) ITC_MAKETYPE(ITC_32F,(n))
+
+#define ITC_64FC1 ITC_MAKETYPE(ITC_64F,1)
+#define ITC_64FC2 ITC_MAKETYPE(ITC_64F,2)
+#define ITC_64FC3 ITC_MAKETYPE(ITC_64F,3)
+#define ITC_64FC4 ITC_MAKETYPE(ITC_64F,4)
+#define ITC_64FC(n) ITC_MAKETYPE(ITC_64F,(n))
+
+#define ITC_AUTO_STEP  0x7fffffff
+#define ITC_WHOLE_ARR  itcSlice( 0, 0x3fffffff )
+
+#define ITC_MAT_CN_MASK          ((ITC_CN_MAX - 1) << ITC_CN_SHIFT)
+#define ITC_MAT_CN(flags)        ((((flags) & ITC_MAT_CN_MASK) >> ITC_CN_SHIFT) + 1)
+#define ITC_MAT_DEPTH_MASK       (ITC_DEPTH_MAX - 1)
+#define ITC_MAT_DEPTH(flags)     ((flags) & ITC_MAT_DEPTH_MASK)
+#define ITC_MAT_TYPE_MASK        (ITC_DEPTH_MAX*ITC_CN_MAX - 1)
+#define ITC_MAT_TYPE(flags)      ((flags) & ITC_MAT_TYPE_MASK)
+#define ITC_MAT_CONT_FLAG_SHIFT  14
+#define ITC_MAT_CONT_FLAG        (1 << ITC_MAT_CONT_FLAG_SHIFT)
+#define ITC_IS_MAT_CONT(flags)   ((flags) & ITC_MAT_CONT_FLAG)
+#define ITC_IS_CONT_MAT          ITC_IS_MAT_CONT
+#define ITC_MAT_TEMP_FLAG_SHIFT  15
+#define ITC_MAT_TEMP_FLAG        (1 << ITC_MAT_TEMP_FLAG_SHIFT)
+#define ITC_IS_TEMP_MAT(flags)   ((flags) & ITC_MAT_TEMP_FLAG)
+
+
+/****************************************************************************************\
+*                                    Sequence types                                      *
+\****************************************************************************************/
+#define CV_SEQ_ELTYPE_GENERIC        0
 
 inline int itcRound(double a)
 {
@@ -208,7 +275,8 @@ typedef struct ItcMemStoragePos
 }ItcMemStoragePos;
 
 /*********************************** Sequence *******************************************/
-#define ITC_SEQ_MAGIC_VAL             0x42990000
+#define ITC_SEQ_MAGIC_VAL             0x42990000  //稠密序列  队列，栈，向量
+#define ITC_SET_MAGIC_VAL             0x42980000	 //稀疏序列  图，点集，哈希表
 typedef struct ItcSeqBlock
 {
     struct ItcSeqBlock*  prev; /* previous sequence block */
@@ -219,7 +287,7 @@ typedef struct ItcSeqBlock
     char*  data;              /* pointer to the first element of the block */
 }ItcSeqBlock;
 
-#define CV_TREE_NODE_FIELDS(node_type)                          \
+#define ITC_TREE_NODE_FIELDS(node_type)                          \
 	int       flags;         /* micsellaneous flags */          \
 	int       header_size;   /* size of sequence header */      \
 struct    node_type* h_prev; /* previous sequence */        \
@@ -231,8 +299,8 @@ struct    node_type* v_next  /* 2nd next sequence */
    Read/Write sequence.
    Elements can be dynamically inserted to or deleted from the sequence.
 */
-#define CV_SEQUENCE_FIELDS()                                            \
-    CV_TREE_NODE_FIELDS(ItcSeq);                                         \
+#define ITC_SEQUENCE_FIELDS()                                            \
+    ITC_TREE_NODE_FIELDS(ItcSeq);                                         \
     int       total;          /* total number of elements */            \
     int       elem_size;      /* size of sequence element in bytes */   \
     char*     block_max;      /* maximal bound of the last block */     \
@@ -244,9 +312,31 @@ struct    node_type* v_next  /* 2nd next sequence */
 
 typedef struct ItcSeq
 {
-    CV_SEQUENCE_FIELDS()
+    ITC_SEQUENCE_FIELDS()
 }
 ItcSeq;
 
-#define CV_TYPE_NAME_SEQ             "opencv-sequence"
-#define CV_TYPE_NAME_SEQ_TREE        "opencv-sequence-tree"
+#define ITC_TYPE_NAME_SEQ             "opencv-sequence"
+#define ITC_TYPE_NAME_SEQ_TREE        "opencv-sequence-tree"
+
+/*********************************** Chain/Countour *************************************/
+
+typedef struct ItcChain
+{
+	ITC_SEQUENCE_FIELDS()
+		ItcPoint  origin;
+}ItcChain;
+
+#define ITC_CONTOUR_FIELDS()  \
+	ITC_SEQUENCE_FIELDS()     \
+	ItcRect rect;             \
+	int color;               \
+	int reserved[3];
+
+typedef struct ItcContour
+{
+	ITC_CONTOUR_FIELDS()
+}
+ItcContour;
+
+typedef ItcContour ItcPoint2DSeq;
